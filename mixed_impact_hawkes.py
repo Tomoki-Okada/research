07352f9578e356ff53a425e7b2347256a034bca6
Tomoki Okada <t-okada0811@keio.jp>
2017-10-15 15:25:44 +0900

    mixed_impact_hawkes.py

--- mixed_impact_hawkes.py
+++ mixed_impact_hawkes.py
@@ -17,6 +17,10 @@ class MIH():
         """
         self.q = data['q']
         self.T = data['T']
+        self.n = data['n']
+        self.x_0 = data['x_0']
+        self.delta_0 = data['delta_0']
+        self.D_0 = data['D_0']
         self.nu = data['nu']
         self.epsilon = data['epsilon']
         self.rho = data['rho']
@@ -106,50 +110,65 @@ class MIH():
                     +self.e(self.T-t)*Sigma+self.g(self.T-t)
         return (first_block + second_block + third_block + forth_block)/self.q
 
-    def phi_ita(t):
-        return 1/(2*(2+rho*(T-t))*(1+np.exp(-ita*(T-t))+nu*rho*(T-t)*zeta(ita*(T-t)) \ 
-                     +beta/rho(2+rho*(T-t)*(1+zeta(ita*(T-t))+nu*rho*(T-t)*omega(ita*(T-t)))))
+#==============================================================================
+
+    def phi_ita(self, t):
+        return 1/(2*(2+self.rho*(self.T-t)))*(1+np.exp(-self.ita*(self.T-t)) \
+                    +self.nu*self.rho*(self.T-t)*self.zeta(self.ita*(self.T-t)) \
+                    +self.beta/self.rho(2+self.rho*(self.T-t)*(1+self.zeta(self.ita*(self.T-t)) \
+                    +self.nu*self.rho*(self.T-t)*self.omega(self.ita*(self.T-t)))))
     
-    def Phi_0(s,t):
-        first_term = (beta/rho+nu/2*(1/2-beta/rho))*(np.exp(-beta*s)-np.exp(-beta*t))/beta
-        second_term = (1-nu)*(1-beta/rho)*np.exp(-beta*T)/rho*(L(rho,beta,T-s)-L(rho,beta,T-t))
-        third_term = nu/4*((T-t)*np.exp(-beta*s))-(T-t)*np.exp(-beta*t)
+    def Phi_0(self, s,t):
+        first_term = (self.beta/self.rho+self.nu/2*(1/2-self.beta/self.rho))*(np.exp(-self.beta*s)-np.exp(-self.beta*t))/self.beta
+        second_term = (1-self.nu)*(1-self.beta/self.rho)*np.exp(-self.beta*self.T) \
+                    /self.rho*(self.L(self.rho,self.beta,self.T-s)-self.L(self.rho,self.beta,self.T-t))
+        third_term = self.nu/4*((self.T-t)*np.exp(-self.beta*s))-(self.T-t)*np.exp(-self.beta*t)
         return first_term + second_term + third_term
         
-    def Phi_ita(s,t):
-        first_term = 1/2*(1/rho+nu/ita)*(np.exp(-beta*s)-np.exp(-beta*t))
-        second_term = np.exp(-beta*T)/(2*rho)*(1+nu*(rho-2*beta)/ita+beta/ita*(1-nu*rho/ita))*(L(rho,beta,T-s)-L(rho,beta,T-t))
-        third_term = np.exp(-beta*T)/(2*rho)*(1-nu*rho/ita-beta/ita*(1-nu*rho/ita))*(L(rho,alpha,T-s)-L(rho,alpha,T-t))
+    def Phi_ita(self, s,t):
+        first_term = 1/2*(1/self.rho+self.nu/self.ita)*(np.exp(-self.beta*s)-np.exp(-self.beta*t))
+        second_term = np.exp(-self.beta*self.T)/(2*self.rho)*(1+self.nu*(self.rho-2*self.beta) \
+                    /self.ita+self.beta/self.ita*(1-self.nu*self.rho/self.ita)) \
+                    *(self.L(self.rho,self.beta,self.T-s)-self.L(self.rho,self.beta,self.T-t))
+        third_term = np.exp(-self.beta*self.T)/(2*self.rho)*(1-self.nu*self.rho/self.ita-self.beta/self.ita \
+                    *(1-self.nu*self.rho/self.ita))*(self.L(self.rho,self.alpha,self.T-s)-self.L(self.rho,self.alpha,self.T-t))
         return first_term + second_term + third_term
         
     def delta_t():
         return delta_0*np.exp(-beta*t)+np.exp(-beta*t)*Theta(chi_t)
         
     def Theta(i):
+        return None
         
-    def Delta_X_0_OW():
-        return -x_0/(2*rho*T)
+    def Delta_X_0_OW(self):
+        return -self.x_0/(2*self.rho*self.T)
     
-    def Delta_X_T_OW():
-        return -x_0/(2*rho*T)
+    def Delta_X_T_OW(self):
+        return -self.x_0/(2*self.rho*self.T)
         
-    def Delta_X_T_OW(n):
-        dt = T/n
-        return -rho*x_0*dt/(2+rho*T)
+    def Delta_X_t_OW(self):
+        dt = self.T/self.n
+        return -self.rho*self.x_0*dt/(2+self.rho*self.T)
            
-    def Delta_X_trend_0():
-        return (delta_0*m_1/2*rho*(2+rho*T*(1+zeta(ita*T)+nu*rho*T*omega(ita*T)))-(1+rho*T)*q*D_0)/((2+rho*T)*(1-epsilon))
+    def Delta_X_trend_0(self):
+        return (self.delta_0*self.m_1/2*self.rho*(2+self.rho*self.T*(1+self.zeta(self.ita*self.T) \
+                +self.nu*self.rho*self.T*self.omega(self.ita*self.T)))-(1+self.rho*self.T)*self.q*self.D_0) \
+                /((2+self.rho*self.T)*(1-self.epsilon))
         
-    def Delta_X_trend_T():
-        first_term = delta_0*m_1/(2*rho)*((2+rho*T*(1+zeta(ita*T)+nu*rho*T*omega(ita*T)))/(2*rho*T)-2*rho*Phi_ita(0,T)-2*np.exp(-beta*T))
-        second_term = q*D_0/(2+rho*T)
-        return (first_term + second_term)/(1 - epsilon)
+    def Delta_X_trend_T(self):
+        first_term = self.delta_0*self.m_1/(2*self.rho)*((2+self.rho*self.T*(1+self.zeta(self.ita*self.T) \
+                    +self.nu*self.rho*self.T*self.omega(self.ita*self.T)))/(2*self.rho*self.T)-2*self.rho*self.Phi_ita(0,self.T) \
+                    -2*np.exp(-self.beta*self.T))
+        second_term = self.q*self.D_0/(2+self.rho*self.T)
+        return (first_term + second_term)/(1 - self.epsilon)
     
-    def Delta_X_trend_t(n):
-        dt = T/n
-        first_term = delta_0*m_1/(2*rho)*((2+rho*T*(1+zeta(ita*T)+nu*rho*T*omega(ita*T)))/(2*rho*T)-2*rho*Phi_ita(0,T)-2*phi_ita(t)*np.exp(-beta*T))*rho*dt 
-        second_term = q*D_0/(2+rho*T)*rho*dt
-        return (first_term + second_term)/(1 - epsilon)
+    def Delta_X_trend_t(self, t):
+        dt = self.T/self.n
+        first_term = self.delta_0*self.m_1/(2*self.rho)*((2+self.rho*self.T*(1+self.zeta(self.ita*self.T) \
+                    +self.nu*self.rho*self.T*self.omega(self.ita*self.T)))/(2*self.rho*self.T)-2*self.rho*self.Phi_ita(0,t) \
+                    -2*self.phi_ita(t)*np.exp(-self.beta*t))*self.rho*dt 
+        second_term = self.q*self.D_0/(2+self.rho*self.T)*self.rho*dt
+        return (first_term + second_term)/(1 - self.epsilon)
         
     def Delta_X_dyn_0():
         return 0
@@ -161,7 +180,7 @@ class MIH():
         return None
 
 if __name__ == '__main__':
-    data = {'q':100,'T':1,'nu':0.3,'epsilon':0.3,'rho':25,'iota_c':2,'kappa_inf':12, \
+    data = {'q':100,'T':1,'n':10,'x_0':-500,'nu':0.3,'epsilon':0.3,'rho':25,'iota_c':2,'kappa_inf':12, \
             'ita':0,'m_1':50,'m_2':100,'alpha_tilda':5,'alpha_2':5,'beta':20}
     cost = Cost()
     cost.set_up(data)
